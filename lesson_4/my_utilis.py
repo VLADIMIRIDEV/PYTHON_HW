ddb4196c670edbd50c976250cd080d794a4b44ed
VLADIMIR DOLGOPOLOV <allisposible@mail.ru>
2022-06-04 19:22:01 +0800

    ДЗ 4

--- lesson_4/my_utilis.py
+++ lesson_4/my_utilis.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 from requests import get
 
 
-def currency_rates(currency: str):
+def currency_rates(currency):
 	url = 'http://www.cbr.ru/scripts/XML_daily.asp'
 	currency = currency.upper()
 	response = get(url)
@@ -23,3 +23,4 @@ def currency_rates(currency: str):
 if __name__ == '__main__':
 	print(currency_rates('usd'))
 	print(currency_rates('Eur'))
+	print(currency_rates('cny'))
